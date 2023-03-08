582c4e0e25a60a6a7bc04fb66bd7e401f92aad05
Imran Shaikh <84342691+Imran660@users.noreply.github.com>
2023-03-08 21:01:32 +0530

    updated the code

--- index.js
+++ index.js
@@ -1,74 +1,53 @@
-// import statements
+//import statements
 const express = require("express");
-const dotenv = require("dotenv");
 const path = require("path");
-//config
-dotenv.config();
-//server init
+
+//config statements
+
+//server or app init
 const server = express();
 
 //server middlewares
+// server.use('/user/details',(req,res,next)=>{
+//     const {key}=req.query;
+//     if(key==12789) next()
+//     else res.status(401).send("Invalid user")
+// })
+// server.use("/user/cart", (req, res, next) => {
+//   const { key } = req.query;
+//   if (key == 12789) next();
+//   else res.status(401).send("Invalid user");
+// });
+const checkUser = (req, res, next) => {
+  const { key } = req.query;
+  if (key == 12789) next();
+  else res.status(401).send("Invalid user");
+};
 server.set("views", path.join(__dirname, "views"));
 server.set("view engine", "ejs");
-// server.use("/user-data", (req, res, next) => {
-//   //we can take input or data from the user in three ways
-//   // 1. params 2. query and 3. body
-//   console.log(process.env.PASSWORD);
-//   const {username,password}=req.query;
-//   if(username=="imran" && password==process.env.PASSWORD){
-//     next()
-//   }else{
-//     res.status(401).send("Unauthorised user, please check your inputs")
-//   }
-// });
 
-// const checkUser = (req, res, next) => {
-//   //we can take input or data from the user in three ways
-//   // 1. params 2. query and 3. body
-//   console.log(process.env.PASSWORD);
-//   const { username, password } = req.query;
-//   if (username == "imran" && password == process.env.PASSWORD) {
-//     next();
-//   } else {
-//     res.status(401).send("Unauthorised user, please check your inputs");
-//   }
-// };
+//server routes or api or services
+server.get("/", (req, res) => {
+  // res.send("hey users, welcome to my express app!..")
+  res.render("index");
+});
 
-//server routes or services
-//Four http methods
-// GET- To read the data
-// POST- To create the data
-// PUT/PATCH To update the data
-// DELETE To delete the data
+server.get("/user/details", checkUser, (req, res) => {
+  res.send("Private details");
+});
 
-//status code
-// success
-// 200 - OK - default
-// 201 - Ok, created
-// 304 - Ok, Not modified - default
-// Error
-// 400 - Bad Request
-// 401 - Unauthorised  - default
-// 403 - forbidden
-// 404 - Not found - default
-// 500 - Internal server error
+server.get("/user/cart", checkUser, (req, res) => {
+  res.send("Card details");
+});
 
-// server.get("/", (req, res) => {
-//   res.render("index", { name: req.query.name });
-// });
+server.get("/user", (req, res) => {
+  res.send("hey user, Imran shaikh !..");
+});
 
-// server.get("/signin",(req,res)=>{
-//     res.render("signin")
-// })
+server.get("/user/:name", (req, res) => {
+  const { name } = req.params;
+  res.render("user", { user: name });
+});
 
-// server.get("/user/:id", (req, res) => {
-//   console.log(req.params);
-//   res.send("hey user and your id is " + req.params.id);
-// });
-
-// server.get("/user-data", checkUser, (req, res) => {
-//   res.send("User key is " + 14652);
-// });
-
-//server start
-server.listen(8000, () => console.log("server started..."));
+//server listen or start
+server.listen(8000, () => console.log("server running on port 8000"));
